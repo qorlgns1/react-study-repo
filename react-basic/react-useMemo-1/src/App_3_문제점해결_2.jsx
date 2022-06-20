fc85eec96d9a8efca5acffd73156a1ab51635866
qorlgns1 <rlgns0610@gmail.com>
2022-06-21 03:55:00 +0900

    react useMemo 공부하면서 여러가지 테스트해보기

---
+++ react-basic/react-useMemo-1/src/App_3_문제점해결_2.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useMemo } from "react";
+
+function 부하() {
+  let s = 0;
+  for (let i = 0; i < 1000000000; i++) {
+    s += i;
+  }
+  return s;
+}
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [countTwo, setCountTwo] = useState(0);
+
+  const handleCountUp = (e) => {
+    setCount(count + 1);
+    console.log(count);
+  };
+  const handleCountUpTwo = (e) => {
+    setCountTwo(countTwo + 1);
+    console.log(countTwo);
+  };
+
+  console.log("랜더링!!");
+
+  // const result = useMemo(() => {
+  //   return 부하();
+  // }, [countTwo]);
+
+  // 콘솔에 aa를 찍어본 결과 undefined가 나왔다.
+  // const result = useMemo(
+  //   (aa) => {
+  //     console.log("aa", aa);
+  //     return 부하();
+  //   },
+  //   [countTwo]
+  // );
+
+  // useMemo 이렇게 사용도 가능하다.
+  const result = useMemo(부하, [countTwo]);
+
+  return (
+    <div className="App">
+      <h1>계산 결과 : {result}</h1>
+      <div>{count}</div>
+      <button onClick={handleCountUp}>up!</button>
+      <div>{countTwo}</div>
+      <button onClick={handleCountUpTwo}>up!</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-basic/react-useMemo-1/src/App_4.jsx
@@ -0,0 +1,151 @@
+import React, { useRef, useState, useEffect, useMemo } from "react";
+
+function CreateUser({ username, email, onChange, onCreate }) {
+  return (
+    <div>
+      <input
+        name="username"
+        placeholder="계정명"
+        onChange={onChange}
+        value={username}
+      />
+      <input
+        name="email"
+        placeholder="이메일"
+        onChange={onChange}
+        value={email}
+      />
+      <button onClick={onCreate}>등록</button>
+    </div>
+  );
+}
+
+function User({ user, onRemove, onToggle }) {
+  useEffect(() => {
+    console.log("컴포넌트가 화면에 나타남");
+    return () => {
+      console.log("컴포넌트가 화면에서 사라짐");
+    };
+  }, []);
+  return (
+    <div>
+      <b
+        style={{
+          cursor: "pointer",
+          color: user.active ? "green" : "black",
+        }}
+        onClick={() => onToggle(user.id)}
+      >
+        {user.username}
+      </b>
+      &nbsp;
+      <span>({user.email})</span>
+      <button onClick={() => onRemove(user.id)}>삭제</button>
+    </div>
+  );
+}
+
+function UserList({ users, onRemove, onToggle }) {
+  return (
+    <div>
+      {users.map((user) => (
+        <User
+          user={user}
+          key={user.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
+      ))}
+    </div>
+  );
+}
+
+function countActiveUsers(users) {
+  console.log("활성 사용자 수를 세는중...");
+  return users.filter((user) => user.active).length;
+}
+
+function App() {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+  });
+  const { username, email } = inputs;
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: "velopert",
+      email: "public.velopert@gmail.com",
+      active: true,
+    },
+    {
+      id: 2,
+      username: "tester",
+      email: "tester@example.com",
+      active: false,
+    },
+    {
+      id: 3,
+      username: "liz",
+      email: "liz@example.com",
+      active: false,
+    },
+  ]);
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    setUsers(users.concat(user));
+
+    setInputs({
+      username: "",
+      email: "",
+    });
+    nextId.current += 1;
+  };
+
+  const onRemove = (id) => {
+    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+    // = user.id 가 id 인 것을 제거함
+    setUsers(users.filter((user) => user.id !== id));
+  };
+  const onToggle = (id) => {
+    setUsers(
+      users.map((user) =>
+        user.id === id ? { ...user, active: !user.active } : user
+      )
+    );
+  };
+
+  // step 1 useMemo를 사용하지않아 input을 바꿔도 계속 함수가 호출됨
+  // const count = countActiveUsers(users);
+
+  // step 2 useMemo를 사용하여 users의 값이 변화가 있을 때만 함수를 호출
+  const count = useMemo(() => countActiveUsers(users), [users]);
+
+  return (
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <div>활성사용자 수 : {count}</div>
+    </>
+  );
+}
+
+export default App;
