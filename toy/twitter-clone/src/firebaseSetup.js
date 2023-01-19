09ed5a9f7e33deeff668c5f3734c88571a641f6a
qorlgns1 <rlgns0610@gmail.com>
2023-01-20 00:04:56 +0900

    firebase database를 이용해서 데이터 읽고 쓰기

--- toy/twitter-clone/src/firebaseSetup.js
+++ toy/twitter-clone/src/firebaseSetup.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import * as firebase from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,4 +22,6 @@ const firebaseConfig = {
 const app = firebase.initializeApp(firebaseConfig);
 
 export const authService = getAuth(app);
+export const databaseService = getFirestore(app);
+
 // const analytics = getAnalytics(app);
--- toy/twitter-clone/src/routes/Home.js
+++ toy/twitter-clone/src/routes/Home.js
@@ -1,7 +1,47 @@
-import React from "react";
+import { databaseService } from "firebaseSetup";
+import React, { useState } from "react";
+import { collection, addDoc, getDocs } from "firebase/firestore";
 
-function Home() {
-  return <div>Home</div>;
-}
+const Home = () => {
+  const [nweet, setNweet] = useState("");
+  const onSubmit = async (event) => {
+    event.preventDefault();
 
+    try {
+      const docRef = await addDoc(collection(databaseService, "nweets"), {
+        first: "Ada",
+        last: "Lovelace",
+        born: 1815,
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+
+    const querySnapshot = await getDocs(collection(databaseService, "nweets"));
+    querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data()}`, doc.data());
+    });
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+    </div>
+  );
+};
 export default Home;
